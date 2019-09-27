f433ecec22a3a3410ede35c8ecb693392615b7f4
Roneet Kumar <roneetparida@gmail.com>
2019-09-27 00:21:06 -0400

    build

---
+++ build/precache-manifest.d958555a00b2270faae15c1278b418e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f12a22f61d2a6112fd89dc24cf398c",
+    "url": "/notified/index.html"
+  },
+  {
+    "revision": "bda865a0c68a1654e3ad",
+    "url": "/notified/static/css/main.3931eb41.chunk.css"
+  },
+  {
+    "revision": "902e9cc7fd0b3c96cfbb",
+    "url": "/notified/static/js/2.658747d0.chunk.js"
+  },
+  {
+    "revision": "bda865a0c68a1654e3ad",
+    "url": "/notified/static/js/main.33e5c0ee.chunk.js"
+  },
+  {
+    "revision": "e8186432ce003996bd36",
+    "url": "/notified/static/js/runtime-main.599f0cae.js"
+  }
+]);
